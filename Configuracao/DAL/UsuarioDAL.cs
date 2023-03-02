b2dec6a360a8f571c111110ad0643d2246701940
A.Barbosa <18252705+arbarbosa1979@users.noreply.github.com>
2023-03-01 21:30:17 -0300

    Update UsuarioDAL.cs

--- Configuracao/DAL/UsuarioDAL.cs
+++ Configuracao/DAL/UsuarioDAL.cs
@@ -129,7 +129,37 @@ namespace DAL
         }
         public void Alterar(Usuario _usuario)
         {
+            SqlConnection cn = new SqlConnection();
 
+            try
+            {
+                cn.ConnectionString = Conexao.StringDeConexao;
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandText = @"INSERT INTO USUARIO(NOME, NOME_USUARIO, CPF_USUARIO, EMAIL_USUARIO, SENHA_USUARIO, ATIVO)
+
+                                    VALUES(@NOME, @NOME_USUARIO, @CPF_USUARIO, @EMAIL_USUARIO, @SENHA_USUARIO, @ATIVO)";
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.Parameters.AddWithValue("@NOME", _usuario.Nome);
+                cmd.Parameters.AddWithValue("@NOME_USUARIO", _usuario.NomeUsuario);
+                cmd.Parameters.AddWithValue("@CPF_USUARIO", _usuario.CPF);
+                cmd.Parameters.AddWithValue("@EMAIL_USUARIO", _usuario.Email);
+                cmd.Parameters.AddWithValue("@SENHA_USUARIO", _usuario.Senha);
+                cmd.Parameters.AddWithValue("@ATIVO", _usuario.Ativo);
+
+                cn.Open();
+                cmd.ExecuteScalar();
+
+                //SqlCommand cmd = new SqlCommand("SELECT * FROM USUARIO ORDER BY Nome", cn);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao tentar inserir um usuário no banco: " + ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
         }
         public void Excluir(int _id)
         {
